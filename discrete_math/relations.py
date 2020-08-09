f62d2eb46b0842e067805954d073179a4785b43a
Gilad Barnea <giladbrn@gmail.com>
2020-08-09 19:59:10 +0300

    Sun 09 Aug 2020 19:59:10 IDT

--- discrete_math/relations.py
+++ discrete_math/relations.py
@@ -1,17 +1,18 @@
 from itertools import product as prod
-from typing import Callable
+from typing import Callable, Union
 from operator import eq, le, lt, ge, gt, ne  # equal, lower-equal, lower-than, greater-equal, greater-than, not-equal
 from random import randint
+import inspect
 
 cartesian_prod = lambda v: prod(v, v)
 universal_rel = lambda a, b: True
 empty_rel = lambda a, b: False
 identity_rel = lambda a, b: a == b
-
+opposite_rel = lambda a, b:a == -b
 
 # inverse_rel = lambda a,b:
-
-def relate(v, rel: Callable[[int, int], bool], quiet=True):
+R = Callable[[int, int], bool]
+def relate(v, rel: R, quiet=True):
     """Creates a relation. In code terms, just a filter on the cartesian product.
     Examples:
     
@@ -26,99 +27,158 @@ def relate(v, rel: Callable[[int, int], bool], quiet=True):
     >>> relate(V, universal_rel) == V_cart_prod
     True
     """
+    if not quiet:
+        print(f'relation(... ({len(v)}), {inspect.getsourcelines(rel)})')
     relation = set()
     for x, y in cartesian_prod(v):
         if rel(x, y):
             relation.add((x, y))
             if not quiet:
-                print(f'<x,y> = <{x},{y}>')
+                print(f'\t<x={x}, y={y}>')
+    if not quiet:
+        print()
     return relation
 
 
-def relate_sqr(v, rel: Callable[[int, int], bool], quiet=True):
-    """Computes the squared relation"""
+def relate_sqr(v, rel, quiet=True):
+    """Computes the squared relation.
+    
+    >>> r2 = relate_sqr(randset(30), opposite_rel)
+    >>> r = relate(randset(30), identity_rel)
+    >>> r == r2
+    True
+
+    >>> r2 = relate_sqr({1, 2}, {(1, 1), (2, 2)})
+    identity_rel
+    
+    """
     r2 = set()
+    if callable(rel):
+        satisfies = rel
+    else:
+        satisfies = lambda a,b:(a,b) in rel
+    
     for x, y in cartesian_prod(v):
         for z in v:
-            if rel(x, z) and rel(z, y):
+            if satisfies(x, z) and satisfies(z, y):
                 r2.add((x, y))
                 if not quiet:
-                    print(f'<x,y> = <{x},{y}> (z: {z})')
+                    print(f'<x={x}, y={y}> (z={z})')
+    if not callable(rel):
+        try:
+            known_rels = dict(
+                        universal_rel=universal_rel,
+                        empty_rel=empty_rel,
+                        identity_rel=identity_rel,
+                        opposite_rel=opposite_rel,
+            )
+            print(next(name for name, rel in known_rels.items() if r2 == relate(v,rel)))
+        except StopIteration:
+            pass
     return r2
 
 
-def is_symmetric(v, rel: Callable[[int, int], bool]):
+def is_symmetric(v, rel: R,quiet=True):
     """A relation is symmetric if for every pair <a,b> in R, also <b,a> in R
     In other words: rel(a,b) is True and rel(b,a) is True
     >>> symmetric = [universal_rel, empty_rel, eq, ne]
-    >>> all(is_symmetric(randset(300), _) for _ in symmetric)
+    >>> all(is_symmetric(randset(100), _) for _ in symmetric)
+    |R|: ...
+    |R|: ...
     True
     
     >>> not_symmetric = [lt, le]
-    >>> any(is_symmetric(randset(300), _) for _ in not_symmetric)
+    >>> any(is_symmetric(randset(100), _) for _ in not_symmetric)
+    |R|: ...
+    |R|: ...
     False
     
     """
-    relation = relate(v, rel)
+    relation = relate(v, rel,quiet=quiet)
     print(f'|R|: {(rel_len := len(relation))} ({(rel_len / len((card_prod := set(cartesian_prod(v))))) * 100}%)')
-    for x, y in card_prod:
-        if rel(y, x) and (x, y) not in relation:
+    for x, y in relation:
+        assert not (rel(y, x) ^ ((y,x) in relation))
+        if not rel(y, x):
+            if not quiet:
+                print(f'<x={x}, y={y}> not in R but <y,x> is in R: fail')
             return False
+        if not quiet:
+            print(f'<x={x}, y={y}> ok (rel(<y={y}, x={x}>) is True)')
     return True
 
 
-def is_anti_symmetric(v, rel: Callable[[int, int], bool]):
+def is_anti_symmetric(v, rel: R,quiet=True):
     """A relation is anti symmetric if for every pair <a,b> in R, <b,a> is not in R
     In other words: rel(a,b) is True but rel(b,a) is False
     >>> anti_symmetric = [lt, empty_rel]
-    >>> all(is_anti_symmetric(randset(300), _) for _ in anti_symmetric)
+    >>> all(is_anti_symmetric(randset(100), _) for _ in anti_symmetric)
+    |R|: ...
+    |R|: ...
     True
     
     >>> not_anti_symmetric = [lambda a,b:b<a**2]
-    >>> any(is_anti_symmetric(randset(300), _) for _ in not_anti_symmetric)
+    >>> any(is_anti_symmetric(randset(100), _) for _ in not_anti_symmetric)
+    |R|: ...
     False
     """
-    relation = relate(v, rel)
+    relation = relate(v, rel,quiet=quiet)
     print(f'|R|: {(rel_len := len(relation))} ({(rel_len / len((card_prod := set(cartesian_prod(v))))) * 100}%)')
-    for x, y in card_prod:
-        if rel(y, x) and (x, y) in relation:
+    for x, y in relation:
+        assert not (rel(y, x) ^ ((y,x) in relation))
+        if rel(y, x):
+            if not quiet:
+                print(f'<x={x}, y={y}> in R but <y={y}, x={x}> is too: fail')
             return False
+        if not quiet:
+            print(f'<x={x}, y={y}> ok (rel(<y={y}, x={x}>) is False)')
     return True
 
 
-def is_reflexive(v, rel: Callable[[int, int], bool]):
+def is_reflexive(v, rel: R,quiet=True):
     """A relation is reflexive if every x in v satisfies rel(x,x)
     >>> reflexive = [universal_rel, identity_rel, le]
-    >>> all(is_reflexive(randset(300), _) for _ in reflexive)
+    >>> all(is_reflexive(randset(100), _) for _ in reflexive)
+    ...
     True
     >>> not_reflexive = [ne, lt, empty_rel]
-    >>> any(is_reflexive(randset(300), _) for _ in not_reflexive)
+    >>> any(is_reflexive(randset(100), _) for _ in not_reflexive)
+    ...
     False
     
     """
     for x in v:
         if not rel(x, x):
+            if not quiet:
+                print(f'<{x},{x}>: fail')
             return False
+        if not quiet:
+            print(f'<{x},{x}>: ok')
     return True
 
 
-def is_anti_reflexive(v, rel: Callable[[int, int], bool]):
+def is_anti_reflexive(v, rel: R,quiet=True):
     """A relation is anti reflexive if no x in v satisfies rel(x,x)
     >>> anti_reflexive = [ne, lt, empty_rel]
-    >>> all(is_anti_reflexive(randset(300), _) for _ in anti_reflexive)
+    >>> all(is_anti_reflexive(randset(100), _) for _ in anti_reflexive)
+    ...
     True
     >>> not_anti_reflexive = [eq, le]
-    >>> any(is_anti_reflexive(randset(300), _) for _ in not_anti_reflexive)
+    >>> any(is_anti_reflexive(randset(100), _) for _ in not_anti_reflexive)
+    ...
     False
     
     """
     for x in v:
         if rel(x, x):
+            if not quiet:
+                print(f'<{x},{x}>: fail')
             return False
+        if not quiet:
+            print(f'<{x},{x}>: ok')    
     return True
 
 
-def is_empty_relation(v, rel: Callable[[int, int], bool]):
+def is_empty_relation(v, rel: R,quiet=True):
     cart_prod = set(cartesian_prod(v))
     for x, y in cartesian_prod(v):
         if rel(x, y) and (x, y) in cart_prod:
@@ -126,7 +186,7 @@ def is_empty_relation(v, rel: Callable[[int, int], bool]):
     return True
 
 
-def is_universal_relation(v, rel: Callable[[int, int], bool]):
+def is_universal_relation(v, rel: R,quiet=True):
     cart_prod = set(cartesian_prod(v))
     for x, y in cartesian_prod(v):
         if rel(x, y) and (x, y) not in cart_prod:
@@ -138,11 +198,15 @@ def randset(length, start=None, stop=None) -> set:
     """Example: randset(10) # returns a set of length 10 containing random numbers ranging from -10 to 10
     """
     if start is None and stop is None:
-        start = -length
-        stop = length
+        if length % 2 == 0:
+            length -= 1
+        half = length // 2
+        start = -half
+        stop = half + 1
+        tmp = set(range(start,stop))
+        return tmp
     tmp = set()
-    while len(tmp) < length and len(tmp) < stop - start:
+    while (tmp_len := len(tmp)) < length and tmp_len < stop - start:
         tmp.add(randint(start, stop))
     return tmp
 
-# universal relation: is_symm((_randset:=randset(300)), lambda a,b:partial(choice,list(_randset)))
