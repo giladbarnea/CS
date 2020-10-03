a22395efff3a8be073b8165e91def064af9d36fc
Gilad Barnea <giladbrn@gmail.com>
2020-10-03 20:26:39 +0300

    Sat 03 Oct 2020 20:26:38 IDT

---
+++ discrete_math/openfiles_exam.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+xdg-open "/home/gilad/Documents/CompSci/discrete_math/Exams/exams_index.odt"
+xdg-open "/home/gilad/Documents/CompSci/20476_discrete_math/Summary/2 summary.pdf"
+xdg-open "/home/gilad/Documents/CompSci/20476_discrete_math/Exams/2019/2019a2_m83.pdf"
+xdg-open "/home/gilad/Documents/CompSci/20476_discrete_math/Summary/3 סיכום בדידה.pdf"
+xdg-open "/home/gilad/Documents/CompSci/20476_discrete_math/Summary/מיכל וולף שיעורים/מפגש 5 2020א.pdf"
+xdg-open "/home/gilad/Documents/CompSci/20476_discrete_math/Summary/קבוצות - סיכום עם תוספות.pdf"
+xdg-open "/home/gilad/Documents/CompSci/discrete_math/תורת הקבוצות דיגיטלי_עם הערות.pdf"
+xdg-open "/home/gilad/Documents/CompSci/20476_discrete_math/Summary"
+Lotion
+if nautilus "/home/gilad/Documents/CompSci/20476_discrete_math/Exams/2020"; then
+
+	nautilus "/home/gilad/Documents/CompSci/20476_discrete_math"
+	nautilus "/home/gilad/Documents/CompSci/discrete_math"
+fi
